4e420b1290259123b194e4ee49835de54275d757
ENO <nblpb2022@outlook.com>
2025-01-30 15:13:57 +0800

    行动2及修正行动1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Finder_World
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
             开篇();
         }
@@ -23,7 +23,8 @@ namespace Finder_World
             Console.WriteLine("“真不该干这份破工作的，都没人来陪我了。”");
             Console.WriteLine("突然，一阵铃声响起......");
             bool 下一步 = true;
-            for (; 下一步 ==true;) 
+            int 感情 = 10;
+            for (; 下一步;)
             {
                 Console.WriteLine("请输入接下来的行动（输入“查看”以获得可执行步骤）");
                 string 行动1 = Console.ReadLine();
@@ -41,37 +42,67 @@ namespace Finder_World
                     Console.WriteLine("“不是的啦，这宇宙实在太...”");
                     Console.WriteLine("此时这位少女脸蛋通红说：“胡说，你在地球和我谈恋爱的时候怎么给我说的，你当时时说你实在太美丽了，即使星辰也要在我黯淡几分。但现在呢，你（脏话）偷偷跑到黑到点嘞外太空耍，你个臭猫，看我今天不给你耳朵扯下来！！！”");
                     Console.WriteLine("此时少女向你跑了过来");
-                    Console.WriteLine("少女用纤细洁白的手捏住了你小小的猫耳，你想疼的喵喵叫");
+                    Console.WriteLine("少女用纤细洁白的手捏住了你小小的猫耳，你疼得喵喵叫");
+                    int 挣脱值 = 0;
+                    for (; 挣脱值 <= 30;) 
+                    {
+                        挣脱值 = 挣脱(挣脱值); 
+                    }
+                    Console.WriteLine("“哼，要不是看你一个在外太空孤独，不然我就把你尾巴也一起拔下来，懂了吗小猫，吴炜！”");
+                    Console.WriteLine("吴炜满是惊恐的答道：“是的，亲爱的老婆大人，婕妤。”");
+                    Console.WriteLine("“早点飞到S4714后回来看我，我才买的别墅很空呢！”说罢，量子实体化电话关掉了。");
+                    感情 = 感情 + 1;
+                    下一步 = false;
+                }
+                else if (行动1 == "走向窗户")
+                {
+                    Console.WriteLine("你若无其事的走向了窗户");
+                    Console.WriteLine();
+                    Console.WriteLine("但");
+                    Console.ReadKey();
+                    Console.WriteLine("你的身后变的冷飕飕的。");
+                    Console.ReadKey();
+                    Console.WriteLine("突然一阵刺骨的寒冷抓住了你的喉咙。");
+                    Console.WriteLine("“大坏猫居然背着我跑了，还不接我电话，看我今天不狠狠收拾你。”");
+                    Console.WriteLine("（刺骨的寒冷与令人清醒的痛苦提醒你做些什么）");
                     int 挣脱值 = 0;
                     for (; 挣脱值 <= 30;)
                     {
-                        Console.WriteLine("（按下“E”键以挣脱）");
-                        ConsoleKeyInfo key4 = Console.ReadKey();
-                        if (key4.Key == ConsoleKey.E)
-                        {
-                            int 再次挣脱 = 挣脱值 + 1;
-                            挣脱值 = 再次挣脱;
-                            Console.WriteLine("你尝试挣扎了一下，但痛觉仍在你的耳朵上");
-                        }
-                        else
-                        {
-                            Console.WriteLine("你并未反抗，但清晰的痛觉催促你做些什么。");
-                        }
-                        Console.WriteLine("“哼，要不是看你一个在外太空孤独，不然我就把你尾巴也一起拔下来，懂了吗小猫，吴炜！”");
-                        Console.WriteLine("吴炜满是惊恐的答道：“是的，亲爱的老婆大人，婕妤。”");
-                        Console.WriteLine("“早点飞到S4714后回来看我，我才买的别墅很空呢！”说罢，量子实体化电话关掉了。");
-                        下一步 = false;
+                        挣脱值 = 挣脱(挣脱值);
                     }
+                    Console.WriteLine("“疼疼疼！，老婆大人”");
+                    Console.WriteLine("“你还知道疼啊，一天天的，吴炜啊,居然敢背着我跑那么远的地方，还不给我说一声！”");
+                    Console.WriteLine("“好的好的，老婆大人，婕妤，我知道错了”吴炜着急忙慌地说");
+                    Console.WriteLine("说罢，婕妤松开了手，吴炜也落地。");
+                    Console.WriteLine("“早点回来啊，我们还要搬家呢”婕妤挂断了电话，吴炜叹叹气，继续行驶起飞船......");
+                    感情 = 感情 - 1;
+                    下一步 = false;
                 }
-                else 
+                else
                 {
                     Console.WriteLine("（叮叮当当的电话使你无法将注意力放在其他事上）");
                 }
+                Console.WriteLine("next time...");
+                Console.ReadKey();
             }
-            Console.WriteLine("Next Time...");
-            Console.WriteLine("(按下任意键以退出...)");
-            Console.ReadKey();
+        }
+        private static int 挣脱(int 挣脱值)
+        {
+            
+                Console.WriteLine("（按下“E”键以挣脱）");
+                ConsoleKeyInfo key4 = Console.ReadKey();
+                if (key4.Key == ConsoleKey.E)
+                {
+                    挣脱值 = 挣脱值 + 1;
+                    Console.WriteLine("你尝试挣扎了一下，但痛觉仍在你的耳朵上");
+                }
+                else
+                {
+                    Console.WriteLine("你并未反抗，但清晰的痛觉催促你做些什么。");
+                }
+                return 挣脱值;
 
+            
         }
     }
 }
